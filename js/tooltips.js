dafdf5b7782d15c23e1dc30ee8519844b8bac9f6
aneqinun <alexdanquinn@gmail.com>
2026-04-28 22:38:13 +0100

    Update tooltips and setting text to be xianxia themed

--- js/tooltips.js
+++ js/tooltips.js
@@ -17,14 +17,14 @@ const tooltips = {
     "Holy Knight": "Obliterate squadrons of enemy soldiers in one go with extraordinary proficiency, while wielding a magically imbued blade. Such a feared unit on the battlefield is paid extremely well.",
     "Lieutenant General": "Feared by nations, obliterate entire armies in a blink of an eye. Roughly every century, only one holy knight is worthy of receiving such an esteemed title.",
 
-    // The Arcane Association
+    // The Arcane Association / Earthly Cultivation
     "Student": "Study the theory of mana and practice basic spells. There is minor pay to cover living costs; however, this is a necessary stage in becoming a mage.",
     "Apprentice Mage": "Under the supervision of a skilled mage, perform basic spells against enemies in battle. Generous pay will be provided to cover living costs.",
     "Master Wizard": "Utilise advanced spells to ravage and destroy entire legions of enemy soldiers. Only a small percentage of mages deserve to attain this role and are rewarded with an insanely high pay.",
     "Archmage": "Blessed with unparalleled talent, perform unbelievable feats with magic at will. It is said that an archmage has enough destructive power to wipe an empire off the map.",
     "Chronomancer": "Specialize in harnessing temporal energies that alter the flow of time with supernatural divinations and otherwordly expertise.",
-    "Chairman": "Spend your days administrating The Arcane Association and investigate the concepts of true immortality. The chairman receives ludicrous amounts of pay daily.",
-    "Imperator": "You wield an unlimited power, making you unstoppable. By ruling with an iron fist, everyone in the Arcane Association has to obey your commands.",
+    "Chairman": "Spend your days guiding the Earthly Cultivation sect and investigating the path to true immortality. The Chairman receives ludicrous amounts of pay daily.",
+    "Imperator": "You wield near-limitless power. By ruling with an iron fist, every cultivator in the Earthly Cultivation sect bows to your command.",
 
     // The Void
     "Corrupted": "Corrupted by Void, you are slowly turning into a slave with no free will, just to serve the Void for the rest of eternity... Can you resist it, or will it consume you forever?",
@@ -62,23 +62,23 @@ const tooltips = {
     "Astral Body": "Refine your body as a spiritual treasure. Increase your lifespan significantly.", // Bodily Refinement - Refine your body as a treasure. Lifespan grows as the red dust washes from your body and spirit.
     "Temporal Dimension": "Cultivate and nurture time as you would a herb or flower. Increase game speed significantly.", // Temporal cultivation - "Like a herb or flower, time, too, may be nurtured and grown to abundance in its season."
 
-    // Dark Magic - Evil Required / Demonic Cultivation
-    "Dark Influence": "Find your inspiration in endless bloodshed. Increase job and skill XP gain..",
-    "Evil Control": "Enslave the ghosts of innocents to increase your own power. Increase Potential gain.",
-    "Intimidation": "Utilise your killing intent to intimidate those with things you want. Reduce item expenses.",
-    "Blood Meditation": "Nourish demons of the spirit and consume them; demon is a more fitting title for yourself. Increase Potential gain.",
-    "Demon's Wealth": "Steal karma from deserving people for your own benefit. Increase job wages.",
-    "Dark Knowledge": "Harvest rare bloodlines from helpless mortals and bolster your own talent. Increase job xp.",
-    "Void Influence": "Devour the thoughts from talented cultivators and skillful mortals alike. Increase job and skill xp.",
-    "Time Loop": "Sacrifice the youth of those around you to speed the passing of time. Increase game speed.",
+    // Dark Magic / Conceptual Arts
+    "Dark Influence": "You hear the whispers of the dao on the wind, hints on matters great and small. Increase job and skill XP gain.", // Inspiration from the Dao
+    "Evil Control": "You develop your daoist domain; the natural energies of heaven and earth begin to respect your new claims. Increase Potential gain.", // Domain of Authority
+    "Intimidation": " At a glance you begin to understand the natural energies and qualities of goods. Reduce item expenses.", // Profound Recognition
+    "Blood Meditation": "When you close your eyes you feel as though you have a golden path before you, growing ever brighter. Greatness is no longer a question for you. Increase Potential gain.", // Great Destiny
+    "Demon's Wealth": "Your karmic weight grows by the day; significant events start naturally to seek you out. Increase job wage.", // Karmic Significance
+    "Dark Knowledge": "They begin to call you 'blessed with talent', though you have never been before. Things start to come easier to you, as though your instincts already know the answers.", // Heavenly Talent
+    "Void Influence": "Unconsciously, your every breath and movement becomes more in tune with the rhythm of the natural dao. Increase job and skill XP.", // Enlightned Tempo
+    "Time Loop": "Your every thought starts to become tinged with the flavour of the natural Dao; smaller matters pass by all the easier. Increase game speed.", // Rarified Mind
 
     // Celestial Powers / Immortal Cultivation
     "Cosmic Longevity": "You have seen it all, from the very beginning to the very end.",
-    "Cosmic Recollection": "Being able to exist in multiple parallel timelines and manipulating you parallel selves, influencing their lives as you see fit.",
+    "Cosmic Recollection": "Exist across multiple timelines and guide your parallel selves, influencing their lives as you see fit.",
     "Essence Collector": "Exploit the unlimited potential of multiverse energies and collect its resources.",
     "Galactic Command": "Absolute power corrupts absolutely.",
 
-    // Almightiness / ? Manifestation
+    // Almightiness / Manifestation
     "Yin Yang": "Born from chaos when the universe was first created, believed to exist in harmony, balancing evil and good.",
     "Parallel Universe": "Self-contained plane of existence, co-existing with one's own, helping you restore fragments of your forgotten power.",
     "Higher Dimensions": "By possesing the power to partially alter the laws of physics and transceding lower dimensional spaces, your existence becomes never-ending.",
